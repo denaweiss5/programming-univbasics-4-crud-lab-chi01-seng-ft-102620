017453226cdf926ca1b88c3da30cc550ee9fea66
Dena Neuman <denaweiss5@gmail.com>
2020-10-02 22:06:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-
+    retrieve_last_element_from_array = ["wow", "Happy", "arrays!"]
+    retrieve_last_element_from_array[2]
 end
 
 def update_element_from_index(array, index_number, element)
