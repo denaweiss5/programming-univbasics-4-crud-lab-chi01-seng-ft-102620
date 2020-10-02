b525838110b33a5fa488d79caa5bccc696672c83
Dena Neuman <denaweiss5@gmail.com>
2020-10-02 21:43:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,11 +21,13 @@ end
 def remove_element_from_end_of_array(array)
   remove_element_from_end_of_array = ["Happy", "Lucky", "arrays!"]
   remove_element_from_end_of_array.pop
-  
+
 end
 
 def remove_element_from_start_of_array(array)
-
+  remove_element_from_start_of_array = ["wow","happy","sad"]
+  remove_element_from_start_of_array.shift
+  
 end
 
 def retrieve_element_from_index(array, index_number)
