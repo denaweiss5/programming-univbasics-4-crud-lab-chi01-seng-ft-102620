25a59fc93c4631595bb8a27ad487bb9dbfa074b0
Dena Neuman <denaweiss5@gmail.com>
2020-10-02 21:04:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   add_element_to_end_of_array = ["Flora", "Tomi", "Dena", "Lisi"]
 
-  add_element_to_end_of_array << ["arrays!"]
+  add_element_to_end_of_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
